80ef229272dbe2fe64c74201ec326208b27cd725
Derek <ChrisDerekBerbo@gmail.com>
2023-12-28 02:22:52 +0800

    Changed the name

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -9,7 +9,7 @@ const homePage = () => {
       <div className='row'>
         <div className='col-lg-12 col-12 p-5 mt-5'>
           <div className='text-fluid mt-5'>
-          <h1 className='display-1 animate__animated animate__fadeIn animate__delay-1s' style={{ color: 'white' }}>Hi, I Am Chris</h1>
+          <h1 className='display-1 animate__animated animate__fadeIn animate__delay-1s' style={{ color: 'white' }}>Hi, I Am Derek</h1>
           <h1 className='display-1 animate__animated animate__backInLeft animate__delay-2s' style={{ color: 'white' , marginBottom: '15%'}}>Game / Web Developer</h1>
           </div>
         </div>
