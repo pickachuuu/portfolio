6304bc00809cafaa07eda3ec6bf10dd9d2a71567
Derek <ChrisDerekBerbo@gmail.com>
2023-12-30 02:08:40 +0800

    Added new image for workSection and new WIP projecet

--- src/components/workSection.jsx
+++ src/components/workSection.jsx
@@ -3,6 +3,7 @@ import Dino from '../images/1.png';
 import Pong from '../images/2.png';
 import Pongbat from '../images/3.png';
 import Pycal from '../images/4.png';
+import Rental from '../images/5.png';
 
 const WorkSection = () => {
   return (
@@ -44,7 +45,7 @@ const WorkSection = () => {
                     <h5 className="card-title d-inline-block p-1">Pongbat</h5>
                     <span className="badge text-bg-warning ml-5">NEW</span>
                     <p className="card-text">
-                      Basic pong game but added characters and health base gameplay. made using pygame and python.
+                      Created a foundational Pong game, enhancing it with characters and a health-based gameplay mechanic. Implemented using the Pygame library in Python.
                     </p>
                   </div>
                 </div>
@@ -57,7 +58,7 @@ const WorkSection = () => {
                   <div className="card-body">
                     <h5 className="card-title p-1">Dino Jump</h5>
                     <p className="card-text">
-                      Inspired from the chrome dino jump game. Made using pygame and python.
+                      Derived inspiration from the Chrome Dino Jump game and crafted a similar game using Pygame and Python.
                     </p>
                   </div>
                 </div>
@@ -70,7 +71,7 @@ const WorkSection = () => {
                   <div className="card-body">
                     <h5 className="card-title p-1">Pong</h5>
                     <p className="card-text">
-                      Classic pong game remake using pygame and python.
+                      Revitalized the classic Pong game by developing a remake using Pygame and Python.
                     </p>
                   </div>
                 </div>
@@ -88,7 +89,7 @@ const WorkSection = () => {
                   <div className="card-body">
                     <h5 className="card-title d-inline-block p-1">Pycal</h5>
                     <p className="card-text">
-                      A simple calculator app made using tkinter and python.
+                      Crafted a straightforward calculator application using the Tkinter library in Python.
                     </p>
                   </div>
                 </div>
@@ -96,7 +97,31 @@ const WorkSection = () => {
             </div>
           </div>
           <div className="tab-pane fade" id="tab3">
-            {/* Content for tab3 is empty */}
+          {/* Content for tab3 is empty */}
+            <div className="row">
+              <div className="col-md-4 mb-4">
+                <div className="card bg-black text-light" style={{ border: 'none' }}>
+                  <a href="">
+                    <img src={Rental} className="card-img-top cardImg" alt="Placeholder"/>
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title d-inline-block p-1">Car Rental Website</h5>
+                    <span className="badge text-bg-info ml-5">WIP</span>
+                    <div className="progress mb-2" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                      <div className="progress-bar" style={{ width: '25%' }}></div>
+                    </div>
+                    <p className="card-text">
+                      "In progress: Developing a user-friendly Car Rental Website enabling account creation, login, and realistic car rental simulations."
+                    </p>
+                    <p className='lead'>Current progress</p>
+                    <ul>
+                      <li>Log-in</li>
+                      <li>Register</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
