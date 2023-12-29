5fb5e6e136747b7ef50fdde8e4e2d7626b019ddc
Derek <ChrisDerekBerbo@gmail.com>
2023-12-30 02:07:52 +0800

    Changed title

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -10,7 +10,7 @@ const homePage = () => {
         <div className='col-lg-12 col-12 p-5 mt-5'>
           <div className='text-fluid mt-5'>
           <h1 className='display-1 animate__animated animate__fadeIn animate__delay-1s' style={{ color: 'white' }}>Hi, I Am Derek</h1>
-          <h1 className='display-1 animate__animated animate__backInLeft animate__delay-2s' style={{ color: 'white' , marginBottom: '15%'}}>Game / Web Developer</h1>
+          <h1 className='display-1 animate__animated animate__backInLeft animate__delay-2s' style={{ color: 'white' , marginBottom: '15%'}}>Asipiring full stack developer</h1>
           </div>
         </div>
         <div className='col-lg-5 col-12 p-5'>
