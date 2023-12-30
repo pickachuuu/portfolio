e65304786c05c308aa88850108d2e584f14d6439
Derek <ChrisDerekBerbo@gmail.com>
2023-12-30 17:37:19 +0800

    fixed typo

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import self from '../images/me.jpg';
 import banner from '../images/banner.png'
 
 const homePage = () => {
@@ -10,7 +9,7 @@ const homePage = () => {
         <div className='col-lg-12 col-12 p-5 mt-5'>
           <div className='text-fluid mt-5'>
           <h1 className='display-1 animate__animated animate__fadeIn animate__delay-1s' style={{ color: 'white' }}>Hi, I Am Derek</h1>
-          <h1 className='display-1 animate__animated animate__backInLeft animate__delay-2s' style={{ color: 'white' , marginBottom: '15%'}}>Asipiring full stack developer</h1>
+          <h1 className='display-1 animate__animated animate__backInLeft animate__delay-2s' style={{ color: 'white' , marginBottom: '15%'}}>Aspiring full stack developer</h1>
           </div>
         </div>
         <div className='col-lg-5 col-12 p-5'>
